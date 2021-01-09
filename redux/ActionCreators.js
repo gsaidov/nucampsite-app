53aa1dbe39ecbbebb70b2de558f0f28439ba4685
gsaidov <gulomjonsaidov@gmail.com>
2021-01-09 12:50:31 -0800

    Post Commonts Modal

--- redux/ActionCreators.js
+++ redux/ActionCreators.js
@@ -30,6 +30,25 @@ export const commentsFailed = (errMess) => ({
   payload: errMess,
 });
 
+export const postComment = (campsiteId, rating, author, text) => (dispatch) => {
+  const newComment = {
+    campsiteId,
+    rating,
+    author,
+    text,
+  };
+  var date = new Date();
+  newComment.date = date.toISOString();
+  setTimeout(() => {
+    dispatch(addComment(newComment));
+  }, 2000);
+};
+
+export const addComment = (comment) => ({
+  type: ActionTypes.ADD_COMMENT,
+  payload: comment,
+});
+
 export const addComments = (comments) => ({
   type: ActionTypes.ADD_COMMENTS,
   payload: comments,
--- redux/comments.js
+++ redux/comments.js
@@ -1,13 +1,18 @@
 import * as ActionTypes from "./ActionTypes";
-
 export const comments = (state = { errMess: null, comments: [] }, action) => {
   switch (action.type) {
+    case ActionTypes.ADD_COMMENT:
+      let comment = action.payload;
+      comment.id = state.comments.length;
+      return {
+        ...state,
+        errMess: null,
+        comments: state.comments.concat(comment),
+      };
     case ActionTypes.ADD_COMMENTS:
       return { ...state, errMess: null, comments: action.payload };
-
     case ActionTypes.COMMENTS_FAILED:
       return { ...state, errMess: action.payload };
-
     default:
       return state;
   }
